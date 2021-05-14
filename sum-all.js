427e33b46ca108219b3a9f684517cceec046b8f7
archy <andre.katsuragawa@gmail.com>
2021-05-14 12:25:45 -0300

    Add sum-all resolution

--- sum-all.js
+++ sum-all.js
@@ -3,7 +3,14 @@ Return the sum of those two numbers plus the sum of all the numbers between them
 For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10. */
 
 function sumAll(arr) {
-    return 1;
-  }
-  
-  sumAll([1, 4]);
+    let sum = 0;
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    for (let n = min; n <= max; n++) {
+        sum += n;
+    }
+    return sum;
+}
+
+const result = sumAll([4, 2]);
+console.log(result);
